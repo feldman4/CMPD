697a24f9a5c9a279138acec8c74dc215736655b0
feldman <feldman4@mit.edu>
2016-06-30 16:52:26 -0700

    working

--- start.py
+++ start.py
@@ -6,8 +6,7 @@ import random
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.debug=True
-socketio = SocketIO()
-socketio.init_app(app)
+socketio = SocketIO(app)
 
 phrases = []
 
@@ -19,6 +18,7 @@ lastword = ''
 def index():
     return redirect(url_for('base'))
 
+
 @app.route('/base')
 def base():
     JJ = [p.split(' ')[0] for p in phrases]
@@ -26,34 +26,41 @@ def base():
         enemy=url_for('static', filename='images/cyclops.jpg'))
 
 
+@app.route('/versus')
+def versus():
+    JJ = [p.split(' ')[0] for p in phrases]
+    return render_template('index.html', wordbank=JJ,
+        enemy=url_for('static', filename='images/cyclops.jpg'))
+
+
 @socketio.on('insult', namespace='/base')
 def reply(insult):
-    print session
     global flag, lastword
     JJ = [p.split(' ')[0] for p in phrases]
     NN = [p.split(' ')[-1] for p in phrases]
     if flag == 'JJ':
-      emit('update_wordbank', {'wordbank': NN})
-      flag = 'NN'
-      lastword = insult['insult']
+        emit('update_wordbank', {'wordbank': NN})
+        flag = 'NN'
+        lastword = insult['insult']
     else:
-      emit('update_wordbank', {'wordbank': JJ})
-      flag = 'JJ'
+        emit('update_wordbank', {'wordbank': JJ[:10]})
+        flag = 'JJ'
 
 
-      phrase = cmpd.similar_phrase(insult['insult'])
-      phrase = cmpd.singular(phrase)
-      if phrase == insult['insult']:
-          phrase = random.choice(phrases)
+        # phrase = cmpd.similar_phrase(insult['insult'])
+        # phrase = cmpd.singular(phrase)
+        # if phrase == insult['insult']:
+        #   phrase = random.choice(phrases)
 
-      phrase = random.choice(phrases)
-      retort = {'insult': lastword + ' ' + insult['insult'],
+        phrase = random.choice(phrases)
+        retort = {'insult': lastword + ' ' + insult['insult'],
                        'reply': phrase}
-  
-      emit('retort', retort)
-      with open('record', 'a') as fh:
-        import time
-        fh.write('%f\n%s %s\n' % (time.time(), lastword, insult['insult']))
+        print retort
+        emit('retort', retort, namespace='/base')
+
+        # with open('record', 'a') as fh:
+        #   import time
+        #   fh.write('%f\n%s %s\n' % (time.time(), lastword, insult['insult']))
 
 if __name__ == '__main__':
     phrases = cmpd.load_DIDB_pairs()
