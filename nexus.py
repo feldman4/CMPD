c26075183b4f298418db9ddb2c5198d7933354f2
feldman <feldman4@mit.edu>
2016-07-28 14:06:57 -0400

    fuck

--- nexus.py
+++ nexus.py
@@ -155,7 +155,7 @@ def send_encounter(message):
 if __name__ == '__main__':
 
     cmpd_web.app = app
-    cmpd_web.json.key = cmpd_web.make_gspread_json()
+    cmpd_web.json_key = cmpd_web.make_gspread_json()
 
     socketio.run(app)
 
